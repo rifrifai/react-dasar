de0ce3c2527d18406bc1187cc8027f1ce15f3b60
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-05-14 09:26:11 +0700

    useState

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
+
 function Button() {
+  // counter adl var, setcounter adl fungsi utk merubah var counter sedangkan usestate 0 adl nilai awal
+  const [counter, setCounter] = useState(0);
+
+  let newCounter = 0;
   function clickHandler(nama) {
-    console.info(`Saya diKlik ${nama}`);
+    newCounter = counter + 1;
+    console.info(`Saya diKlik ${newCounter} kali`);
+    setCounter(newCounter);
   }
 
   return (
     <div>
       {/* tanda () akan lngsung dirender oleh reactjs */}
-      <button onClick={() => clickHandler("Tyas")}>Click Me! 🔘</button>
+      <button onClick={clickHandler}>I'm clicked {counter} times 🔼</button>
     </div>
   );
 }
